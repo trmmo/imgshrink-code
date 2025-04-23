4f8687e07bc0872108fa5b80f6d6763e474dfbae
Minh Tu Tran <tutm.b21at199@stu.ptit.edu.vn>
2025-04-24 00:34:03 +0700

    Added running args

--- compare_img.py
+++ compare_img.py
@@ -1,8 +1,19 @@
 import cv2
 import numpy as np
+import argparse
+import hashlib
 
 
-def compare_images(img1_path, img2_path, output_path, selection):
+def hashCal(path, algo='md5'):
+    hasher = hashlib.new(algo)
+
+    with open(path, 'rb') as f:
+        while chunk := f.read(4096):
+            hasher.update(chunk)
+
+    return hasher.hexdigest()
+
+def export_diff(img1_path, img2_path, output_path):
     img1 = cv2.imread(img1_path)
     img2 = cv2.imread(img2_path)
 
@@ -18,32 +29,63 @@ def compare_images(img1_path, img2_path, output_path, selection):
         img2 = cv2.resize(img2, (w, h))
 
     diff = np.zeros_like(img1)
-    count = 0
     for i in range(img1.shape[0]):
         for j in range(img1.shape[1]):
-            # Lấy giá trị pixel
             b1, g1, r1 = img1[i, j]
             b2, g2, r2 = img2[i, j]
             if b1 == b2 and g1 == g2 and r1 == r2:
-                diff[i, j] = [255,255,255]
+                diff[i, j] = [255, 255, 255]
             else:
-                diff[i, j] = [255,0,255]
-                count += 1
+                diff[i, j] = [0, 0, 255]
 
     diff_normalized = cv2.normalize(diff, None, 0, 255, cv2.NORM_MINMAX)
-    if (selection == 1):
-        print(f"2 anh khac nhau {count} diem anh")
+    cv2.imwrite(output_path, diff_normalized)
+    hash_value = hashCal(output_path)
+    print(f"Anh so sanh duoc luu tai: {output_path}, MD5 checksum: {hash_value}")
+
+
+def count_diff(img1_path, img2_path):
+    img1 = cv2.imread(img1_path)
+    img2 = cv2.imread(img2_path)
+
+    if img1 is None or img2 is None:
+        print("Khong the doc anh!!")
+        return
+
+    if img1.shape != img2.shape:
+        print("Hai anh khong cung kich thuoc!!")
+        h = min(img1.shape[0], img2.shape[0])
+        w = min(img1.shape[1], img2.shape[1])
+        img1 = cv2.resize(img1, (w, h))
+        img2 = cv2.resize(img2, (w, h))
+
+    count = 0
+    for i in range(img1.shape[0]):
+        for j in range(img1.shape[1]):
+            b1, g1, r1 = img1[i, j]
+            b2, g2, r2 = img2[i, j]
+            if not(b1 == b2 and g1 == g2 and r1 == r2):
+                count += 1
+    print(f"Hai anh khac nhau {count} pixel.")
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path1', default="fakepath1.jpg", type=str, help='Duong dan anh 1')
+    parser.add_argument('--path2', default="fakepath2.jpg", type=str, help='Duong dan anh 2')
+    parser.add_argument('--pathout', default="fakepathout.jpg", type=str, help='Duong dan anh 2')
+    parser.add_argument('--action', default="nothing", type=str, help='Duong dan anh 2')
+    args = parser.parse_args()
+    # image = cv2.imread(args.pathin)
+    # width = image.shape[1]
+    # height = image.shape[0]
+    # resized_image = cv2.resize(cv2.resize(image, (args.width, args.height)), (width, height))
+    #
+    # cv2.imwrite(filename=args.pathout, img=resized_image)
+    # selection = args.action
+    if (args.action == "export"):
+        export_diff(args.path1, args.path2, args.pathout)
+    elif (args.action == "count"):
+        count_diff(args.path1, args.path2)
     else:
-        cv2.imwrite(output_path, diff_normalized)
-        print(f"Anh so sanh duoc luu tai: {output_path}")
-
-
-img_path1 = input("Nhap vi tri file anh 1: ")
-img_path2 = input("Nhap vi tri file anh 2: ")
-selection = int(input(
-"""Nhap tuy chon:
-    1: Dem so pixel khac nhau
-    2: Xuat anh so sanh 2 anh
-"""))
-
-compare_images(img_path1, img_path2, "diff.jpg", selection)
+        print("Success is not final, failure is not fatal: it is the courage to continue that counts. :D")
--- img_resize.py
+++ img_resize.py
@@ -1,13 +1,31 @@
 import cv2
+import argparse
+import hashlib
 
-path_in = input("Nhap duong dan file anh: ")
-image = cv2.imread(path_in)
 
-width = image.shape[1]
-height = image.shape[0]
+def hashCal(path, algo='md5'):
+    hasher = hashlib.new(algo)
 
-resized_image = cv2.resize(cv2.resize(image, (int(width / 2), int(height / 2))), (width, height))
+    with open(path, 'rb') as f:
+        while chunk := f.read(4096):  # đọc từng khối nhỏ để tránh dùng quá nhiều RAM
+            hasher.update(chunk)
 
-path_out = "attacked.jpg"
-cv2.imwrite(filename=path_out, img=resized_image)
-print(f"Da sua anh, duong dan file: {path_out}")
+    return hasher.hexdigest()
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pathin', default="fakepathin.jpg", type=str, help='Duong dan anh input')
+    parser.add_argument('--pathout', default="fakepathout.jpg", type=str, help='Duong dan anh output')
+    parser.add_argument('--width', default=100, type=int, help='Chieu ngang anh')
+    parser.add_argument('--height', default=100, type=int, help='Chieu cao anh')
+    args = parser.parse_args()
+
+    image = cv2.imread(args.pathin)
+    width = image.shape[1]
+    height = image.shape[0]
+    resized_image = cv2.resize(cv2.resize(image, (args.width, args.height)), (width, height))
+
+    cv2.imwrite(filename=args.pathout, img=resized_image)
+    hash_value = hashCal(args.pathout)
+    print(f"Anh moi luu tai: {args.pathout}, MD5 checksum: {hash_value}")
+    # print(f"")
